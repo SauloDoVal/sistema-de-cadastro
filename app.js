b8078946ea3129aa5e5ce6118fe1e2c3285b9a81
Saulo S. R. do Val <saulosrdoval@gmail.com>
2023-03-03 19:01:39 +0000

    Voltando atras para antes do banana banana banana

--- app.js
+++ app.js
@@ -2,4 +2,3 @@ console.log("Rodando o Sistema de Gerenciamento de Jogos")
 console.log("aplicação está se conectando ao banco de dados")
 
 console.log("midificando uma parada para ver se vai")
-console.log("banana, banana, banana")
