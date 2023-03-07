3f7f3a7a195953cf9a906728b74d323953cf7898
Saulo S. R. do Val <saulosrdoval@gmail.com>
2023-03-07 12:35:32 +0000

    Corrigindo um negócio ai que tava zoando no texto do console

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-console.log("Rodando o Sistema de Gerenciamento de Jogos")
-console.log("aplicação está se conectando ao banco de dados")
+console.log('Rodando o Sistema de Gerenciamento de Jogos')
+console.log('aplicação está se conectando ao banco de dados')
 
-console.log("midificando uma parada para ver se vai")
+console.log('modificando uma parada para ver se vai')
